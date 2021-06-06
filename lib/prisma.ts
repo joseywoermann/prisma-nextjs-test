a34fcfce39ef56d54a399946c1d17002e1965df6
joseywoermann <jcw05@gmx.de>
2021-06-06 17:33:51 +0200

    weg mit dem Müll

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,23 +1,5 @@
-// lib/prisma.ts
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client"
 
-let prisma: PrismaClient
-
-prisma = new PrismaClient()
+let prisma = new PrismaClient()
 
 export default prisma
-/*import { PrismaClient } from '@prisma/client'
-
-let prisma: PrismaClient
-
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient()
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient()
-  }
-  prisma = global.prisma
-}
-
-export default prisma
-*/
